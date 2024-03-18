577da8d2e574f19d26d13e37127b6241eb567e88
limon8884 <dbokatenko@nes.ru>
2024-03-18 16:50:50 -0500

    upd

--- src/train_param_checkpoints.py
+++ src/train_param_checkpoints.py
@@ -4,11 +4,17 @@ from torchvision.models import resnet18
 
 def save_params_example(filename):
     model = resnet18()
-    dict_to_save = {}
+    list_of_params = []
+    dict_scheme = {}
+    position = 0
     for name, param in model.named_parameters():
         if 'bn' in name:
-            dict_to_save[name] = param
-    torch.save(dict_to_save, filename)
+            print(name, param.shape)
+            dict_scheme[name] = (position, position + len(param))
+            assert len(param.shape) == 1
+            list_of_params.append(param)
+    vec_to_save = torch.cat(list_of_params, dim=0)
+    torch.save(vec_to_save, filename)
 
 
 if __name__ == '__main__':
